89f70e6e48f8f3b095f52e75eb025138d86976a8
Augusto Guaschi Morato <111884429+Guto06@users.noreply.github.com>
2023-10-08 18:09:46 -0300

    Update FILA.h

--- FILA.h
+++ FILA.h
@@ -10,6 +10,7 @@ typedef struct data {
     int ano;
 } Data;
 
+
 typedef struct tarefa {
     int codigo;
     char nome[30];
@@ -23,6 +24,11 @@ typedef struct nos {
     Tarefa info;
     struct nos* prox;
 } No;
+
+typedef struct lista {
+    No* ini;
+    No* fim;
+} Lista;
 
 typedef struct fila {
     No* ini;
@@ -95,4 +101,78 @@ Fila* liberaFila(Fila* f) {
     return NULL;
 }
 
+// Funções para manipular a lista de tarefas pendentes
+
+Lista* CriaLista() {
+    Lista* l = (Lista*)malloc(sizeof(Lista));
+    l->ini = l->fim = NULL;
+    return l;
+}
+
+void InsereLista(Lista*l, Tarefa t) {
+    No* novo = (No*)malloc(sizeof(No));
+    novo->info = t;
+    novo->prox = NULL;
+
+    if (l->fim == NULL) {
+        l->ini = l->fim = novo;
+    } else {
+        l->fim->prox = novo;
+        l->fim = novo;
+    }
+}
+
+Tarefa RetiraLista(Lista*l) {
+    Tarefa t;
+
+    if (l->ini == NULL) {
+        printf("Lista vazia.\n");
+        // Você pode tratar o erro de outra maneira, se desejar.
+        exit(0);
+    }
+
+    t = l->ini->info;
+    No* temp = l->ini;
+
+    l->ini = l->ini->prox;
+    free(temp);
+
+    if (l->ini == NULL) {
+        l->fim = NULL;
+    }
+
+    return t;
+}
+
+void MoverPendentesParaLista(Fila* fila, Lista* lista) {
+    No* atual = fila->ini;
+
+    while (atual != NULL) {
+        if (atual->info.status == -1) { // Tarefa pendente
+            InsereLista(lista, atual->info);
+            No* temp = atual;
+            atual = atual->prox;
+            free(temp);
+        } else {
+            atual = atual->prox;
+        }
+    }
+
+    // Atualize a fila principal
+    fila->ini = fila->fim = NULL;
+}
+
+void MoverListaParaPendentes(Fila* fila, Lista* lista) {
+    No* atual = lista->ini;
+
+    while (atual != NULL) {
+        InsereFila(fila, atual->info);
+        No* temp = atual;
+        atual = atual->prox;
+        free(temp);
+    }
+
+    // Atualize a lista de tarefas pendentes
+    lista->ini = lista->fim = NULL;
+}
 #endif // FILA_H_INCLUDED
